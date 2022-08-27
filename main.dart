187b954cf878383c6616435866544e6d9d9e567a
Rahafjamal <rjabuhawela16@eng.just.edu.jo>
2022-08-27 22:43:09 +0300

    this is update

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main(){
+    Map RahafInformation = {
+        "first_name" : "Rahaf",
+        "last_name" : "jamal",
+        "age": 23,
+        "familyMembers" : [{
+            "father":"jamal",
+            "mother" : "khadiejeh",
+            "sisters" : ["laila","haneen","loma"],
+            "brother" : "mahmoud",
+    
+        }],
+        "education info":[{
+            "name":"JUST",
+            "faculty" : "engineering",
+        }]
+    };
+
+
+}
